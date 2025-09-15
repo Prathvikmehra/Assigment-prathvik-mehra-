da5ee2570a6d3c22dcdab6cac8fed58ad4232b75
prathvik mehra <prathvik.mehra.cg@gmail.com>
2025-09-15 11:21:16 +0530

    14que

---
+++ c-assigment/patternq15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+   int a; 
+scanf("%d",&a);
+for(int i=1;i<=a;i++){
+    for(int j=1;j<=a-i;j++){
+        printf(" ");
+    }
+
+    for(int k=1;k<=2*i-1;k++){
+        printf("%c",k+'A'-1);
+    }
+
+    printf("\n");
+}
+
+    return 0;
+}
